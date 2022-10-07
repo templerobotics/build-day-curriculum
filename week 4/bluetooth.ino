160e4afe6af166734939bf154801bdf8d513c6eb
Ryan Hodge <ryanhodge240@gmail.com>
2022-10-07 17:25:03 -0400

    fixing bluetooth motor

--- week 4/bluetooth.ino
+++ week 4/bluetooth.ino
@@ -1,15 +1,9 @@
-#define f 16736925  // FORWARD
-#define b 16754775  // BACK
-#define l 16720605  // LEFT
-#define r 16761405  // RIGHT
-#define s 16712445  // STOP
-
 #define ENA 5
 #define ENB 6
-#define IN1 7
-#define IN2 8
-#define IN3 9
-#define IN4 11
+#define LEFT_1 7
+#define LEFT_2 8
+#define RIGHT_1 9
+#define RIGHT_2 11
 #define carSpeed 250
 
 enum MOTION_MODE {
@@ -23,40 +17,40 @@ enum MOTION_MODE {
 void forward(bool debug = false){
 	analogWrite(ENA, carSpeed);
 	analogWrite(ENB, carSpeed);
-	digitalWrite(IN1,LOW);
-	digitalWrite(IN2,HIGH);
-	digitalWrite(IN3,HIGH);
-	digitalWrite(IN4,LOW);
+	digitalWrite(RIGHT_1, LOW);
+	digitalWrite(RIGHT_2, HIGH);
+	digitalWrite(LEFT_1, HIGH);
+	digitalWrite(LEFT_2, LOW);
 	if(debug) Serial.println("Go back!");
 }
 
 void back(bool debug = false){ 
 	analogWrite(ENA, carSpeed);
 	analogWrite(ENB, carSpeed);
-	digitalWrite(IN1,HIGH);
-	digitalWrite(IN2,LOW);
-	digitalWrite(IN3,LOW);
-	digitalWrite(IN4,HIGH);
+	digitalWrite(RIGHT_1, HIGH);
+	digitalWrite(RIGHT_2, LOW);
+	digitalWrite(LEFT_1, LOW);
+	digitalWrite(LEFT_2, HIGH);
 	if(debug) Serial.println("Go forward!");
 }
 
 void left(bool debug = false){
 	analogWrite(ENA,carSpeed);
 	analogWrite(ENB,carSpeed);
-	digitalWrite(IN1,LOW);
-	digitalWrite(IN2,HIGH);
-	digitalWrite(IN3,LOW);
-	digitalWrite(IN4,HIGH); 
+	digitalWrite(RIGHT_1, LOW);
+	digitalWrite(RIGHT_2, HIGH);
+	digitalWrite(LEFT_1, LOW);
+	digitalWrite(LEFT_2, HIGH);
 	if(debug) Serial.println("Go left!");
 }
 
 void right(bool debug = false){
 	analogWrite(ENA,carSpeed);
 	analogWrite(ENB,carSpeed);
-	digitalWrite(IN1,HIGH);
-	digitalWrite(IN2,LOW);
-	digitalWrite(IN3,HIGH);
-	digitalWrite(IN4,LOW);
+	digitalWrite(RIGHT_1, HIGH);
+	digitalWrite(RIGHT_2, LOW);
+	digitalWrite(LEFT_1, HIGH);
+	digitalWrite(LEFT_2, LOW);
 	if(debug) Serial.println("Go right!");
 }
 
@@ -71,7 +65,7 @@ void getBTData() {
 		int bt_data = Serial.read();
 		Serial.println(bt_data);
 		switch(bt_data) {
-			case 102: mov_mode = FORWARD;  break;
+			case 'f': mov_mode = FORWARD;  break;
 			case 'b': mov_mode = BACK;     break;
 			case 'l': mov_mode = LEFT;     break;
 			case 'r': mov_mode = RIGHT;    break;
@@ -95,10 +89,10 @@ void bluetooth_mode() {
 void setup() {
 	mov_mode = STOP;
 	Serial.begin(9600);
-	pinMode(IN1, OUTPUT);
-	pinMode(IN2, OUTPUT);
-	pinMode(IN3, OUTPUT);
-	pinMode(IN4, OUTPUT);
+	pinMode(LEFT_1, OUTPUT);
+	pinMode(LEFT_2, OUTPUT);
+	pinMode(RIGHT_1, OUTPUT);
+	pinMode(RIGHT_2, OUTPUT);
 	pinMode(ENA, OUTPUT);
 	pinMode(ENB, OUTPUT);
 	Serial.println("Program Starting");
